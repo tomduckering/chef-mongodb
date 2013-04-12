56230645857df1f09d3b720cff3a9cf7ca1f8acd
Tom Duckering <tom.duckering@gmail.com>
2013-04-12 16:25:10 +0100

    Explicitly setting version to 1.8.5

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,6 +27,7 @@ needs_mongo_gem = (node.recipe?("mongodb::replicaset") or node.recipe?("mongodb:
 
 # install the mongo ruby gem at compile time to make it globally available
 gem_package 'mongo' do
+  version "1.8.5"
   action :nothing
 end.run_action(:install)
 Gem.clear_paths
