93021e8e777b82b58e43d37ab5153805db35b10f
Tom Duckering <tom.duckering@gmail.com>
2013-05-24 16:30:05 +0100

    Removing another superfluous include of the default recipe.

--- definitions/mongodb.rb
+++ definitions/mongodb.rb
@@ -23,8 +23,6 @@ define :mongodb_instance, :mongodb_type => "mongod" , :action => [:enable, :star
     :bind_ip => nil, :port => 27017 , :logpath => "/var/log/mongodb",
     :dbpath => "/data", :configfile => "/etc/mongodb.conf", :configserver => [],
     :replicaset => nil, :enable_rest => false, :notifies => [] do
-    
-  include_recipe "mongodb::default"
   
   name = params[:name]
   type = params[:mongodb_type]
