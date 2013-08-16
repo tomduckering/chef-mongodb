83427bb3a24a4db406050bfa515c4e3390fa6cd6
Tom Duckering <tom.duckering@gmail.com>
2013-08-17 00:41:24 +0100

    Simplifying the setup of the mongo database.

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,17 +19,15 @@
 # limitations under the License.
 #
 
-
 include_recipe 'mongodb::packages'
 
-if node.recipe?("mongodb::default") or node.recipe?("mongodb")
-  # configure default instance
-  mongodb_instance "mongodb" do
-    mongodb_type "mongod"
-    bind_ip      node['mongodb']['bind_ip']
-    port         node['mongodb']['port']
-    logpath      node['mongodb']['logpath']
-    dbpath       node['mongodb']['dbpath']
-    enable_rest  node['mongodb']['enable_rest']
-  end
+# configure default instance
+mongodb_instance "mongodb" do
+  mongodb_type "mongod"
+  bind_ip      node['mongodb']['bind_ip']
+  port         node['mongodb']['port']
+  logpath      node['mongodb']['logpath']
+  dbpath       node['mongodb']['dbpath']
+  enable_rest  node['mongodb']['enable_rest']
 end
+
