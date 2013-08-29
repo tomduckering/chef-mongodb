605d0f9a4e3be6e3b783f66446f240329e2860cc
Tom Duckering <tom.duckering@gmail.com>
2013-08-29 09:39:59 +0100

    Fixing attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -42,7 +42,7 @@ default[:mongodb][:init_dir] = "/etc/init.d"
 
 default[:mongodb][:init_script_template] = "mongodb.init.erb"
 
-node[:mongodb][:omit_repos] = false
+default[:mongodb][:omit_repos] = false
 
 case node['platform']
 when "freebsd"
